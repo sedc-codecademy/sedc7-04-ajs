df37b6a57b7251bde29cef352403156fb0faad72
dragan <dragang@iborn.net>
2019-01-09 21:17:36 +0100

    Class 1 Finished

--- g2/Class1/Code/script.js
+++ g2/Class1/Code/script.js
@@ -1,36 +1,41 @@
 //console.log("We are live!");
 
-// let myJson = `{
-//     "trainer": "Dragan Gelevski",
-//     "assistant": "Dejan Blazeski",
-//     "students": [
-//       "Bob",
-//       "Samantha",
-//       "Chris",
-//       "Jill",
-//       "Greg"
-//     ],
-//     "academy": "Code"
-//   }`;
-// let jsObject = JSON.parse(myJson);
-// console.log(typeof(myJson));
-// console.log(jsObject.students[0]);
-// document.getElementById("sendRequest")
-// .addEventListener("click", function(){
-//     let xhr = new XMLHttpRequest();
-//     xhr.onload = function(){
-//         console.log("Request is sent!");
-//         if(xhr.status >= 200 && xhr.status < 300){
-//             console.log("Request successful!");
-//             let objectResponse = JSON.parse(xhr.response);
-//             console.log(objectResponse);
-//         } else {
-//             console.log(xhr.responseText);
-//         }
-//     };
-//     xhr.open("GET", "https://raw.githubusercontent.com/sedc-codecademy/sedc7-04-ajs/master/g2/Class1/students.json");
-//     xhr.send();
-// });
+// JSON Example
+let myJson = `{
+    "trainer": "Dragan Gelevski",
+    "assistant": "Dejan Blazeski",
+    "students": [
+      "Bob",
+      "Samantha",
+      "Chris",
+      "Jill",
+      "Greg"
+    ],
+    "academy": "Code"
+  }`;
+let jsObject = JSON.parse(myJson);
+console.log(typeof(myJson));
+console.log(jsObject.students[0]);
+
+// Plain JS request
+document.getElementById("sendRequest")
+.addEventListener("click", function(){
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        console.log("Request is sent!");
+        if(xhr.status >= 200 && xhr.status < 300){
+            console.log("Request successful!");
+            let objectResponse = JSON.parse(xhr.response);
+            console.log(objectResponse);
+        } else {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.open("GET", "https://raw.githubusercontent.com/sedc-codecademy/sedc7-04-ajs/master/g2/Class1/students.json");
+    xhr.send();
+});
+
+// JQuery request
 $(document).ready(function(){
     document.getElementById("sendRequest")
     .addEventListener("click", function(){
@@ -40,8 +45,6 @@ $(document).ready(function(){
                 console.log("Request successful!");
                 let responseObject = JSON.parse(response);
                 console.log(responseObject);
-                myResponse = responseObject;
-                document.getElementById("asistent").innerText = myResponse.assistant;
             },
             error: function(response){
                 console.log("The request has failed: " + response.status);
@@ -49,4 +52,28 @@ $(document).ready(function(){
             }
         })
     });
+})
+
+// Astronauts example
+$(document).ready(function(){
+document.getElementById("sendRequest")
+.addEventListener("click", function(){
+    $.ajax({
+        url: "http://api.open-notify.org/astros.json",
+        success: function(response){
+            console.log("Request successful!");
+            // let responseObject = JSON.parse(response);
+            console.log(response);
+            let element = document.getElementById("astros");
+            element.innerHTML = "";
+        for(let i = 0; i < response.people.length; i++){
+            element.innerHTML += `<li> ${response.people[i].name} </li>`;
+        }  
+        },
+        error: function(response){
+            console.log("The request has failed: " + response.status);
+            console.log(response.responseText);
+        }
+    })
+});
 })
