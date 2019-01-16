7f593ff00e16937291b778955bd281c82c446457
igeto <igetom@gmail.com>
2019-01-16 16:08:30 +0100

    add exercises for class 3

---
+++ g6/03-functions/scripts/script.js
@@ -0,0 +1,11 @@
+// Write a pure function that will add ski at the end of given last names in an array
+let lastNames = ['Petrov', 'Sulejmanov', 'Cavkov', 'Donev', 'Mitev', 'Artemov']
+
+// Write a recursive function for calculating factoriel of a given number
+// 6! === 720
+
+// Write a recursive function for calculating sum of given array of numbers
+let numArray = [5 ,12, 6, 2, 6, 765, 2, 12, 36, 4, 67, 2, 1, 85]
+
+// Write a closure function that will calculate square of a given number
+// Write the same closure but this time return the closure
