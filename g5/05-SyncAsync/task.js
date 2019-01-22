0fa53dca489ae33bc02b76fb437780728da24200
Igor Yoshevski <gigo_j44@hotmail.com>
2019-01-22 19:52:03 +0100

    Homework task

---
+++ g5/05-SyncAsync/task.js
@@ -0,0 +1,5 @@
+//Use the image from the URL: http://tiny.cc/zfim2y
+//Using setInterval or/and setTimeout to make the Robot to move from far left to far right on the screen
+//The movement should be done from a JS function, one move by a time.
+//Add two buttons, (left, right) = when user click on them the robot will move one step left (back) or one step //right (forward).
+
