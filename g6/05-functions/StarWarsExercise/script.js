4a8becc4599d22e9b6ef51700da399eab2c19385
Igor Mitkovski <Igor.Mitkovski@sedc.mk>
2019-01-23 21:08:59 +0100

    Add code from todays exercies and homework

--- g6/05-functions/StarWarsExercise/script.js
+++ g6/05-functions/StarWarsExercise/script.js
@@ -1,31 +1,28 @@
-const url = 'https://swapi.co/api/people/';
-let previous, next;
-let people = [];
-
+const peopleUrl = 'https://swapi.co/api/people/';
+const planetsUrl = 'https://swapi.co/api/planets/';
+const people = [];
+const planets = [];
 
 $(() => {
-    $('#goHome').on('click', () => {
-        $('#body').html('');
-        $('.logo').removeClass('display-none')
-        $('#table').addClass('display-none')
-        $('#people').addClass('display-none')
-        $('#planets').addClass('display-none')
-    })
+    $('#goHome').on('click', showLogo)
     $('#getPeople').on('click', () => {
-        getCharatersData(url)
+        showPeople()
+        getCharatersData(peopleUrl)
+    })
+    $('#getPlanets').on('click', () => {
+        showPlanets()
+        getPlanetsData(planetsUrl)
     })
 })
 let getCharatersData = (url) => {
-    $('#body').html('');
-    $('.logo').addClass('display-none')
-    $('#table').removeClass('display-none')
-    $('#people').removeClass('display-none')
-    $('#spinner').html('<img class="text-center" src="./assets/spinner3.gif">');
-
     people.length ? 
     populateTable(people) :
-    fetchData(url)
-    
+    fetchData(url)    
+}
+const getPlanetsData = url => {
+    planets.length ? 
+    populatePlanetsTable(planets) :
+    fetchPlanets(url)    
 }
 
 const fetchData = url => {
@@ -41,10 +38,24 @@ const fetchData = url => {
         populateTable(people)
     })
 }
+const fetchPlanets = url => {
+    fetch(url).then(r => r.json())
+    .then(r => { 
+        planets.push(...r.results)
+        return r
+    })
+    .then(r => {
+        console.log(r)
+        r.next ? 
+        fetchPlanets(r.next) : 
+        populatePlanetsTable(planets)
+    })
+}
 
 let populateTable = (people) => {
     $('#spinner').html('')
     $('#body').html('');
+    const formatedPeople = formatData(people)
     for (let person of formatedPeople) {
         $('#body').append(`
                 <tr>
@@ -58,7 +69,60 @@ let populateTable = (people) => {
     }
 }
 
+let populatePlanetsTable = (planets) => {
+    $('#spinner').html('')
+    $('#body').html('');
+    for (let planet of planets) {
+        $('#body').append(`
+                <tr>
+                <td>${planet.name}</td>
+                <td>${planet.diameter}</td>
+                <td>${planet.climate}</td>
+                <td>${planet.terrain}</td>
+                <td>${planet.rotation_period}</td>
+                <td>${planet.population}</td>
+                </tr>
+                `)
+    }
+}
+
+const showLogo = () => {
+    $('#body').html('');
+    $('.logo').removeClass('display-none')
+    $('#table').addClass('display-none')
+    $('#people').addClass('display-none')
+    $('#planets').addClass('display-none')
+}
+const showPeople = () => {
+    $('#body').html('');
+    $('.logo').addClass('display-none')
+    $('#table').removeClass('display-none')
+    $('#people').removeClass('display-none')
+    $('#planets').addClass('display-none')
+    $('#spinner').html('<img class="text-center" src="./assets/spinner3.gif">');
+}
+
+const showPlanets = () => {
+    $('#body').html('');
+    $('.logo').addClass('display-none')
+    $('#table').removeClass('display-none')
+    $('#people').addClass('display-none')
+    $('#planets').removeClass('display-none')
+    $('#spinner').html('<img class="text-center" src="./assets/spinner3.gif">');
+}
+
 const formatData = people => {
+    return people.map(p => {
+        // let obj = Object.assign({}, p, { height: p.height + 'cm', mass: p.mass + 'kg' })
+        let obj = {
+            ...p,
+            height: p.height !== 'unknown' ? p.height + 'cm' : p.height,
+            mass: p.mass !== 'unknown' ? p.mass + 'kg' : p.mass
+        }
+        // p.height += 'cm'
+        // p.mass += 'kg'
+        return obj
+    })
 }
 
 let findCharacter = (data, keyword) => {
