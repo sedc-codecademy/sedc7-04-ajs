708645f92f5021ae3ecfad9517bdbea4a03f2bb5
Igor Mitkovski <Igor.Mitkovski@sedc.mk>
2019-01-26 12:23:03 +0100

    add class 6 exercise

---
+++ g6/06-event-loop/script.js
@@ -0,0 +1,81 @@
+
+// console.log('one')
+// console.log('two')
+// console.log('three')
+// setTimeout(() => {
+//     console.log('four')
+// }, 0)
+// // alert('jhi')
+// console.log('five')
+// console.log('six')
+
+// function one() {
+//     two()
+//     two()
+// }
+// function two() {
+//     three()
+//     three()
+// }
+// function three() {
+//     four()
+//     four()
+// }
+// function four() {
+//     console.log('hello g6')
+// }
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let rand = Math.random() < 0.7
+            return rand ? resolve() : reject()
+        }, ms)
+    })
+}
+
+console.log('one')
+delay(1000).then(() => {
+    console.log('two')
+    return delay(1000)
+}).then(() => {
+    console.log('three')
+    return delay(1000)
+}).then(() => {
+    console.log('four')
+    return delay(1000)
+}).then(() => {
+    console.log('five')
+    return delay(1000)
+}).then(() => {
+    console.log('six')
+    return delay(1000)
+}).catch(() => console.log('error'))
+
+
+// one()
+// setTimeout(() => {
+//     console.log('one')
+//     setTimeout(() => {
+//         setTimeout(() => {
+//             setTimeout(() => {
+//                 setTimeout(() =>{
+//                     console.log('five')
+//                     setTimeout(() => {
+//                         console.log('six')
+//                     }, 1000)
+//                 }, 1000)
+//                 console.log('four')
+//             }, 1000)
+//             console.log('three')
+//         }, 1000)
+//         console.log('two')
+//     }, 1000)
+// }, 1000)
+
+// let result = fetch(url)
+// /**
+//  * some code here...
+//  */
+// let jsonResult = result
+// .then(r => r.json())
+// .then(r => showOnScreen())
