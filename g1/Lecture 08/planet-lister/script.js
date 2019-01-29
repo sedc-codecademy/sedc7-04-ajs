706157cc1b3ee5f23b83a5cce6a2c07af3b9308d
Wekoslav Stefanovski <swekster@gmail.com>
2019-01-29 20:43:21 +0100

    Added loader and paging

--- g1/Lecture 08/planet-lister/script.js
+++ g1/Lecture 08/planet-lister/script.js
@@ -7,26 +7,79 @@ const config = {
     planets: "planets"
 }
 
+// with .then syntax - only first page
+
+// function loadPlanetData() {
+//     const select = document.getElementById("planet-selector");
+
+//     fetch(`${config.baseUrl}/${config.planets}`)
+//         .then(response => response.json())
+//         .then(result => {
+
+//             const planets = result.results.map(planet => ({
+//                 id: getIdFromPlanetUrl(planet.url),
+//                 name: planet.name
+//             }));
+
+//             for (const planet of planets) {
+//                 const option = document.createElement('option');
+//                 option.value = planet.id;
+//                 option.text = planet.name;
+//                 select.add(option);
+//             }
+//         });
+// }
+
+const planets = [];
 
 async function loadPlanetData() {
-    const response = await fetch(`${config.baseUrl}/${config.planets}`);
-    const result = await response.json();
-    
-    const planets = result.results.map(planet => ({
-        id : getIdFromPlanetUrl(planet.url),
-        name: planet.name
-    }));
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
+    loader.style.display = "block";
+    content.style.display = "none";
 
-    console.log(planets);
+    let hasNext = true;
+    let page = 1;
+
+    do {
+        console.log(`Loading page ${page}`);
+        const url = `${config.baseUrl}/${config.planets}?page=${page}`;
+        const response = await fetch(url);
+        const result = await response.json();
+        
+        planets.push(...result.results.map(planet => ({
+            id: getIdFromPlanetUrl(planet.url),
+            name: planet.name
+        })));
+
+        hasNext = result.next !== null;
+        page += 1;
+    } while(hasNext);
+    console.log("Finished loading");
+
+    renderPlanetOptions(planets);
+    loader.style.display = "none";
+    content.style.display = "block";
 }
 
-function getIdFromPlanetUrl (planetUrl) {
+function renderPlanetOptions(planets) {
+    const select = document.getElementById("planet-selector");
+    for (const planet of planets) {
+        const option = document.createElement('option');
+        option.value = planet.id;
+        option.text = planet.name;
+        select.add(option);
+    }
+}
+
+
+function getIdFromPlanetUrl(planetUrl) {
     // // manipulate the string until it yields the ID
     // // remove the first 29 characters; 
     // const tail = planetUrl.substr(29);
     // // remove the last slash
     // return tail.substr(0, tail.lastIndexOf("/"));
-    
+
     // using regex
     // **never write regex in code**
     // ** use https://regex101.com/ **
