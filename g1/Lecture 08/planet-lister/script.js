18732442410005b8cc9417c5f2bf387ee43637e0
Wekoslav Stefanovski <swekster@gmail.com>
2019-01-29 19:52:11 +0100

    Added planet loading

--- g1/Lecture 08/planet-lister/script.js
+++ g1/Lecture 08/planet-lister/script.js
@@ -11,5 +11,26 @@ const config = {
 async function loadPlanetData() {
     const response = await fetch(`${config.baseUrl}/${config.planets}`);
     const result = await response.json();
-    console.log(result);
+    
+    const planets = result.results.map(planet => ({
+        id : getIdFromPlanetUrl(planet.url),
+        name: planet.name
+    }));
+
+    console.log(planets);
+}
+
+function getIdFromPlanetUrl (planetUrl) {
+    // // manipulate the string until it yields the ID
+    // // remove the first 29 characters; 
+    // const tail = planetUrl.substr(29);
+    // // remove the last slash
+    // return tail.substr(0, tail.lastIndexOf("/"));
+    
+    // using regex
+    // **never write regex in code**
+    // ** use https://regex101.com/ **
+    const idRegex = /^.*\/(\d+)\/$/;
+    const match = planetUrl.match(idRegex);
+    return (match) ? match[1] : null;
 }
