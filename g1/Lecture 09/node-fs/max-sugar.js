d225feac0a5badda46f7d595883b77e4096eff07
Wekoslav Stefanovski <swekster@gmail.com>
2019-01-31 22:21:06 +0100

    Added max-sugar exercise

--- g1/Lecture 09/node-fs/max-sugar.js
+++ g1/Lecture 09/node-fs/max-sugar.js
@@ -14,8 +14,28 @@ const fileReader = {
     }
 }
 
-function getMaxSugar() {
-    return -1;
+async function getMaxSugar() {
+    // read the file with the fruit list (and pause until it's done)
+    const fruits = await fileReader.readJson("./fruit-list.json");
+    // get an array of promises of the reading of the files
+    const fruitPromises = fruits
+        // from a fruit's name get the name of the fruit's file
+        .map(fruit => `./fruits/${fruit.toLowerCase()}.json`)
+        // from a name of the fruit's file get a promise for the file's data
+        .map(fruitFile => fileReader.readJson(fruitFile));
+
+    // Promise.all transforms an array of promises to a promise of an array
+    // await pauses until we have the array, and then unwraps the promise
+    const fruitData = await Promise.all(fruitPromises);
+    // reduces the list until we are left with a single element with maximum sugar
+    return fruitData.reduce(
+        // compare the current max with the new contender, and keep the bigger one
+        (max, fruit) => max.sugars > fruit.sugars ? max : fruit, 
+        // ensure that the first element runs good, by creating a current max
+        // with the lowest possible ammount of sugar
+        {sugars: Number.NEGATIVE_INFINITY}
+    );
 }
 
-console.log(getMaxSugar());
+
+getMaxSugar().then(console.log);
