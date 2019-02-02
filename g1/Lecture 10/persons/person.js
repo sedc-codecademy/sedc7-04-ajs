8277766ec8cd3c6a79420a10539651bcaa35ad58
Wekoslav Stefanovski <swekster@gmail.com>
2019-02-02 10:27:09 +0100

    Added first person example

---
+++ g1/Lecture 10/persons/person.js
@@ -0,0 +1,23 @@
+const weko = {
+    firstName: "Wekoslav",
+    lastName: "Stefanovski",
+    age: 0x29,
+    getFullName: function () {
+        console.log(this);
+        return `${this.firstName} ${this.lastName}`;
+    }
+};
+
+const hitByaBus = (person) => {
+    console.log(this);
+    return `${person.firstName} got hit by a bus`;
+}
+
+const jovan = {
+	firstName: "Jovan",
+	lastName: "Jovanovski"
+}
+
+// make this work (using weko's function)
+const yourself = {}
+console.log(yourself.getFullName());
