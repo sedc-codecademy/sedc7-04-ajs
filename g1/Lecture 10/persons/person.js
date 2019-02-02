fd3bd18cb047cb513b54c7a467924dd3a73b1dc6
Wekoslav Stefanovski <swekster@gmail.com>
2019-02-02 11:25:17 +0100

    Added constructors

--- g1/Lecture 10/persons/person.js
+++ g1/Lecture 10/persons/person.js
@@ -1,23 +1,47 @@
-const weko = {
-    firstName: "Wekoslav",
-    lastName: "Stefanovski",
-    age: 0x29,
-    getFullName: function () {
+const g1Students = [];
+
+const firstNames = ["Todor", "Goran", "Jovan", "Vladimir"];
+const lastNames = ["Janevski", "Kuzmanoski", "Jovanovski", "Poposki"];
+
+// with factory function
+// for (let index = 0; index < 4; index++) {
+//     const student = makeMeAPerson(firstNames[index], lastNames[index]);
+//     g1Students.push(student);
+// }
+
+// g1Students.push(makeMeAPerson("Natasha", "Andova"));
+
+// console.log(g1Students.map(student => student.getFullName()));
+
+function makeMeAPerson(firstName, lastName) {
+    const result = {};
+    result.firstName = firstName;
+    result.lastName = lastName;
+    result.getFullName = function () {
         console.log(this);
         return `${this.firstName} ${this.lastName}`;
-    }
-};
+    };
 
-const hitByaBus = (person) => {
-    console.log(this);
-    return `${person.firstName} got hit by a bus`;
+    return result;
 }
 
-const jovan = {
-	firstName: "Jovan",
-	lastName: "Jovanovski"
+// with constructor function
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getFullName = function () {
+        console.log(this);
+        return `${this.firstName} ${this.lastName}`;
+    };
 }
 
-// make this work (using weko's function)
-const yourself = {}
-console.log(yourself.getFullName());
+for (let index = 0; index < 4; index++) {
+    const student = new Person(firstNames[index], lastNames[index]);
+    g1Students.push(student);
+}
+
+g1Students.push(new Person("Natasha", "Andova"));
+
+console.log(g1Students.map(student => student.getFullName()));
+
