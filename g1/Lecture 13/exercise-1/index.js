f4d1d1eca0d1aefddab9b733f8a9018feae81962
Wekoslav Stefanovski <swekster@gmail.com>
2019-02-12 21:01:48 +0100

    Added exercise 2

--- g1/Lecture 13/exercise-1/index.js
+++ g1/Lecture 13/exercise-1/index.js
@@ -1,3 +1,5 @@
+const toTitleCase = require("to-title-case");
+
 const bookTitles = [
     "WomeN At War",
     "The spirit Ring",
@@ -8,9 +10,12 @@ const bookTitles = [
     "Brothers In Arms",
     "Borders Of Infinity",
     "Mirror-dance",
-    "A Civil-Campaign",
+    "A-Civil-Campaign",
     "Diplomatic_Immunity",
     "Gentleman_Jole_And_The_Red_Queen",
     "the curse of chalion",
     "paladinOfSouls"
 ]
+
+const fixedTitles = bookTitles.map(title => toTitleCase(title));
+console.log(fixedTitles);
