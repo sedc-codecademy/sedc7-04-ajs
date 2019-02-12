11251b881acf367200507eaf39fbcceb28fd341b
Wekoslav Stefanovski <swekster@gmail.com>
2019-02-12 19:50:18 +0100

    Added npm examples

---
+++ g1/Lecture 13/exercise-1/index.js
@@ -0,0 +1,16 @@
+const bookTitles = [
+    "WomeN At War",
+    "The spirit Ring",
+    "Shards OF Honor",
+    "Falling free",
+    "The vor Game",
+    "Ethan oF Athos",
+    "Brothers In Arms",
+    "Borders Of Infinity",
+    "Mirror-dance",
+    "A Civil-Campaign",
+    "Diplomatic_Immunity",
+    "Gentleman_Jole_And_The_Red_Queen",
+    "the curse of chalion",
+    "paladinOfSouls"
+]
